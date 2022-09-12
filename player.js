eaba0d6131b7e92486dac1cf5ca917b5a19efc93
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-12 18:30:52 +0900

    edited player

--- player.js
+++ player.js
@@ -1,5 +1,15 @@
 class Player {
   constructor(game){
     this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = 100;
+  }
+  update(){
+
+  }
+  draw(){
+
   }
 }
