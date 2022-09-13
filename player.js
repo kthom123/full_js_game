c55fa1084438cf28c911a7100d5107432c7d67ba
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-13 20:50:37 +0900

    added draw

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-class Player {
+export class Player {
   constructor(game){
     this.game = game;
     this.width = 100;
@@ -9,7 +9,7 @@ class Player {
   update(){
 
   }
-  draw(){
-
+  draw(context){
+    context.fillRect(this.x, this.y, this.width, this.height);
   }
 }
