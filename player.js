0a6983ca698005cfb135219bee0387ca67b4781c
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-14 13:08:48 +0900

    added dog

--- player.js
+++ player.js
@@ -5,13 +5,13 @@ export class Player {
     this.height = 91.3;
     this.x = 0;
     this.y = this.game.height - this.height;
+    this.image = document.getElementById('player');
   }
   update(){
 
   }
   draw(context){
-    context.fillStyle = 'red';
-    context.fillRect(this.x, this.y, this.width, this.height);
-    context.drawImage();
+    context.drawImage(this.image, 0, 0, this.width, this.height,
+      this.x, this.y, this. width, this.height);
   }
 }
