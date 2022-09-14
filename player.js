44342ef93cf2fb4d4c5f65ca8e11ed96e04c9b0f
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-14 15:53:22 +0900

    edited player

--- player.js
+++ player.js
@@ -10,9 +10,11 @@ export class Player {
     this.vy = 0;
     this.weight = 1;
     this.image = document.getElementById('player');
+    this.frameX = 0;
+    this.frameY = 0;
     this.speed = 0;
     this.maxSpeed = 10;
-    this.states = [];
+    this.states = [new Sitting(this)];
     this.currentState = this.states[0];
     this.currentState.enter();
   }
