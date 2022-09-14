ceee3d5b493e386bc5e63aca2594a1b834fdba3f
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-14 13:05:46 +0900

    edited draw

--- player.js
+++ player.js
@@ -4,12 +4,14 @@ export class Player {
     this.width = 100;
     this.height = 91.3;
     this.x = 0;
-    this.y = 100;
+    this.y = this.game.height - this.height;
   }
   update(){
 
   }
   draw(context){
+    context.fillStyle = 'red';
     context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage();
   }
 }
