b6782a7422d13e392d369a85e1c5b1be3d570cf4
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-15 09:18:24 +0900

    edited player.js

--- player.js
+++ player.js
@@ -13,13 +13,16 @@ export class Player {
     this.frameX = 0;
     this.frameY = 0;
     this.maxFrame = 5;
+    this.fps = 20;
+    this.frameInterval = 1000/this.fps;
+    this.frameTimer = 0;
     this.speed = 0;
     this.maxSpeed = 10;
     this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
     this.currentState = this.states[0];
     this.currentState.enter();
   }
-  update(input){
+  update(input, deltaTime){
     this.currentState.handleInput(input);
     // horizontal movement
     this.x += this.speed;
