7af3bd81235ab635f9a4d358ad4bf031255051b5
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-15 09:16:42 +0900

    added deltatime

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ window.addEventListener('load', function(){
       this.player = new Player(this);
       this.input = new InputHandler();
     }
-    update(){
-      this.player.update(this.input.keys);
+    update(deltaTime){
+      this.player.update(this.input.keys, deltaTime);
     }
     draw(context){
       this.player.draw(context);
@@ -30,7 +30,7 @@ window.addEventListener('load', function(){
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.update();
+    game.update(deltaTime);
     game.draw(ctx);
     requestAnimationFrame(animate);
   }
