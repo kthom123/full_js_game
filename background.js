5265e9a2c32aa99b726770c55e8fe5b5500fadbb
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-18 17:02:24 +0900

    edited background

--- background.js
+++ background.js
@@ -34,7 +34,7 @@ export class Background {
     this.layer4 = new Layer(this.game, this.width, this.height, 0.8, this.layer4image);
     this.layer5 = new Layer(this.game, this.width, this.height, 1, this.layer5image);
     this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5];
-  }
+  } // stopped at 8:05:19
   update(){
     this.backgroundLayers.forEach(layer => {
       layer.update();
