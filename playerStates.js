cff31e1abec7e1e78cced00d18d810966c6c04e9
Kyle Thomas <kylejwthomas@gmail.com>
2022-12-26 10:36:25 +0900

    edited all state classes

--- playerStates.js
+++ playerStates.js
@@ -56,60 +56,57 @@ export class Running extends State {
 }
 
 export class Jumping extends State {
-  constructor(player){
-    super('JUMPING');
-    this.player = player;
+  constructor(game){
+    super('JUMPING', game);
   }
   enter(){
-    if (this.player.onGround()) this.player.vy -= 27;
-    this.player.frameX = 0;
-    this.player.maxFrame = 6;
-    this.player.frameY = 1;
+    if (this.game.player.onGround()) this.game.player.vy -= 27;
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 6;
+    this.game.player.frameY = 1;
   }
   handleInput(input){
-    if (this.player.vy > this.player.weight){
-      this.player.setState(states.FALLING, 1);
+    if (this.game.player.vy > this.game.player.weight){
+      this.game.player.setState(states.FALLING, 1);
     } else if (input.includes('Enter')){
-      this.player.setState(states.ROLLING, 2);
+      this.game.player.setState(states.ROLLING, 2);
     }
   }
 }
 
 export class Falling extends State {
-  constructor(player){
-    super('FALLING');
-    this.player = player;
+  constructor(game){
+    super('FALLING', game);
   }
   enter(){
-    this.player.frameX = 0;
-    this.player.maxFrame = 6;
-    this.player.frameY = 2;
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 6;
+    this.game.player.frameY = 2;
   }
   handleInput(input){
-    if (this.player.onGround()){
-      this.player.setState(states.RUNNING, 1);
+    if (this.game.player.onGround()){
+      this.game.player.setState(states.RUNNING, 1);
     }
   }
 }
 
 export class Rolling extends State {
-  constructor(player){
+  constructor(game){
     super('ROLLING');
-    this.player = player;
   }
   enter(){
-    this.player.frameX = 0;
-    this.player.maxFrame = 6;
-    this.player.frameY = 6;
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 6;
+    this.game.player.frameY = 6;
   }
   handleInput(input){
-    if (!input.includes('Enter') && this.player.onGround()){
-      this.player.setState(states.RUNNING, 1);
-    } else if (!input.includes('Enter') && !this.player.onGround()){
-      this.player.setState(states.FALLING, 1);
+    if (!input.includes('Enter') && this.game.player.onGround()){
+      this.game.player.setState(states.RUNNING, 1);
+    } else if (!input.includes('Enter') && !this.game.player.onGround()){
+      this.game.player.setState(states.FALLING, 1);
     } else if (input.includes('Enter') && input.includes('ArrowUp') &&
-    this.player.onGround()) {
-      this.player.vy -= 27;
+    this.game.player.onGround()) {
+      this.game.player.vy -= 27;
     }
   }
 }
