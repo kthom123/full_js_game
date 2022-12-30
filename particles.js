5a9d4f57ba9b2f3144c29dd041b3651c868e1622
Kyle Thomas <kylejwthomas@gmail.com>
2022-12-30 15:40:30 -0500

    added trail

--- particles.js
+++ particles.js
@@ -6,7 +6,7 @@ class Particle {
   update(){
     this.x -= this.speedX + this.game.speed;
     this.y -= this.speedY;
-    this.size *= 0.95;
+    this.size *= 0.97;
     if (this.size < 0.5) this.markedForDeletion = true;
   }
 }
@@ -37,7 +37,7 @@ export class Fire extends Particle {
   constructor(game, x, y){
       super(game);
       this.image = document.getElementById('fire');
-      this.size = Math.random() * 50 + 50;
+      this.size = Math.random() * 100 + 50;
       this.x = x;
       this.y = y;
       this.speedX = 1;
@@ -48,12 +48,13 @@ export class Fire extends Particle {
   update(){
     super.update();
     this.angle += this.va;
+    this.x += Math.sin(this.angle * 5);
   }
   draw(context){
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.angle);
-    context.drawImage(this.image, 0, 0, this.size, this.size);
+    context.drawImage(this.image, -this.size * 0.5, -this.size * 0.5, this.size, this.size);
     context.restore();
   }
 }
