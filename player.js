8e5e3d8c763698ecc4e6ec9585d1aed038f81a26
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-03 15:50:04 -0500

    adding splash particle

--- player.js
+++ player.js
@@ -29,12 +29,17 @@ export class Player {
     if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
     else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
     else this.speed = 0;
+    // horizontal boundaries
     if (this.x < 0) this.x = 0;
     if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
     // vertical movement
+    if (this.y > this.game.height - this.height - this.game.groundMargin) this.y =
+    this.game.height - this.height - this.game.groundMargin;
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
+    // vertical boundaries
+
     // sprite animation
     if (this.frameTimer > this.frameInterval){
       this.frameTimer = 0;
