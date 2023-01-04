937e5d6e97fb19c6498385f5baeb8065b313bc22
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-04 17:40:06 -0500

    added extra splash

--- particles.js
+++ particles.js
@@ -32,9 +32,9 @@ export class Dust extends Particle {
 export class Splash extends Particle {
   constructor(game, x, y){
     super(game);
-    this.size - Math.random() * 100 + 100;
-    this.x = x;
-    this.y = y;
+    this.size = Math.random() * 100 + 100;
+    this.x = x - this.size * 0.4;
+    this.y = y - this.size * 0.5;
     this.speedX = Math.random() * 6 - 3;
     this.speedY = Math.random() * 2 + 2;
     this.gravity = 0;
@@ -45,7 +45,9 @@ export class Splash extends Particle {
     this.gravity += 0.1;
     this.y += this.gravity;
   }
-
+  draw(context){
+    context.drawImage(this.image, this.x, this.y, this.size, this.size);
+  }
 }
 
 export class Fire extends Particle {
