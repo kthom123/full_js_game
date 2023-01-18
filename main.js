10a3eae05e6d6b4e941b7dcceaccb3fb632cc97f
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-18 15:05:33 +0900

    added collision

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ window.addEventListener('load', function(){
       this.UI = new UI(this);
       this.enemies = [];
       this.particles = [];
+      this.collisions = [];
       this.maxParticles = 50;
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
@@ -53,6 +54,11 @@ window.addEventListener('load', function(){
         if(this.particles.length > this.maxParticles) {
           this.particles = this.particles.slice(0, this.maxParticles);
         }
+        // handle collision sprites
+        this.collisions.forEach((collision, index) => {
+          collision.update(deltaTime);
+          if(collision.markedForDeletion) this.collisions.splice(index, 1);
+        });
     }
     draw(context){
         this.background.draw(context);
@@ -63,8 +69,11 @@ window.addEventListener('load', function(){
         this.particles.forEach(particle => {
           particle.draw(context);
         });
+        this.collisions.forEach(collision => {
+          collision.draw(context);
+        });
         this.UI.draw(context);
-    }
+    } // paused at 9:16:42 ------------------------------------
     addEnemy(){
       if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
       else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
