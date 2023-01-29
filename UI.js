41a030159c79410031b5c7c1ca5e8e7b3a481e6b
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-29 19:08:22 +0900

    fixed seconds display

--- UI.js
+++ UI.js
@@ -5,6 +5,11 @@ export class UI {
     this.fontFamily = 'Helvetica';
   }
   draw(context){
+    context.save();
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = 'white';
+    context.shadowBlur = 0;
     context.font = this.fontSize + 'px' + this.fontFamily;
     context.textAlign = 'left';
     context.fillStyle = this.game.fontColor;
@@ -12,7 +17,7 @@ export class UI {
     context.fillText('Score: ' + this.game.score, 20, 50);
     //timer
     context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
-    context.fillText('Time: ' + this.game.time, 20, 80);
+    context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
     //game over messages
     if (this.game.gameOver){
       context.textAlign = 'center';
@@ -31,5 +36,6 @@ export class UI {
         * 0.5, this.game.height * 0.5 + 20);
       }
     }
+    context.restore();
   }
 }
