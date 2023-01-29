3bc17e8a3b2410ce1d6ed4fde9a34303622ad7c6
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-29 18:03:36 +0900

    removed rectangles

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ window.addEventListener('load', function(){
       this.maxParticles = 50;
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
-      this.debug = true;
+      this.debug = false;
       this.score = 0;
       this.player.currentState = this.player.states[0];
       this.player.currentState.enter();
