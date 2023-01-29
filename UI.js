b42ea8668bc6f9094c2bcc707c6742cddc705d85
Kyle Thomas <kylejwthomas@gmail.com>
2023-01-29 18:15:24 +0900

    updated game over message

--- UI.js
+++ UI.js
@@ -11,6 +11,13 @@ export class UI {
     //score
     context.fillText('Score: ' + this.game.score, 20, 50);
     //timer
-    context.font = this.fontSize * 0.8 + ' px' + this.fontFamily;
+    context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+    context.fillText('Time: ' + this.game.time, 20, 80);
+    //game over messages
+    if (this.game.gameOver){
+      context.textAlign = 'center';
+      context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+      context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5);
+    }
   }
 }
